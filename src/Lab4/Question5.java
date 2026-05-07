8d16ae5c28997334368104074910c41a6ff56889
tuguldur <tuguldur@tuguldurs-MacBook-Pro.local>
2026-05-07 17:50:19 -0500

    [update]

---
+++ src/Lab4/Question5.java
@@ -0,0 +1,25 @@
+package Lab4;
+
+public class Question5 {
+    static int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i]);
+        }
+        return dp[n-1];
+    }
+    static void main() {
+        int[] input = new int[]{2, 7, 9, 3, 1};
+        IO.println(rob(input));
+    }
+}
